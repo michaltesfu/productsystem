c8123542d43b55d8b00fe1a9464a286d5cb9b8df
michal tesfu <michaltesfu2@gmail.com>
2024-07-27 16:10:14 -0500

    add

--- src/main/java/miu/edu/ProductSystem/model/Product.java
+++ src/main/java/miu/edu/ProductSystem/model/Product.java
@@ -1,6 +1,31 @@
 package miu.edu.ProductSystem.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "products data")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long productId;
+
+    private String name;
+    private double price;
+    private int quantity;
+    private LocalDate supplied;
+
+    @ManyToOne
+    @JoinColumn(name = "supplier_id")
+    private Supplier supplier;
 
 
 }
