c0e3705ccbc0a35c27160d043454ffd6b4882bc3
michal tesfu <michaltesfu2@gmail.com>
2024-07-27 15:44:19 -0500

    changed

--- src/main/java/miu/edu/ProductSystem/model/Product.java
+++ src/main/java/miu/edu/ProductSystem/model/Product.java
@@ -2,8 +2,5 @@ package miu.edu.ProductSystem.model;
 
 public class Product {
 
-    private int id;
-    private String name;
-    private String description;
-    private double price;
+
 }
--- src/main/java/miu/edu/ProductSystem/model/Supplier.java
+++ src/main/java/miu/edu/ProductSystem/model/Supplier.java
@@ -2,19 +2,5 @@ package miu.edu.ProductSystem.model;
 
 public class Supplier {
 
-    private int id;
-    private String name;
 
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
 }
