8a0ab66e5144274368454f0c815c4e682607f427
michal tesfu <michaltesfu2@gmail.com>
2024-07-27 15:32:59 -0500

    cut

---
+++ src/main/java/miu/edu/ProductSystem/ProductSystemApplication.java
@@ -0,0 +1,13 @@
+package miu.edu.ProductSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/miu/edu/ProductSystem/ProductSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.edu.ProductSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
