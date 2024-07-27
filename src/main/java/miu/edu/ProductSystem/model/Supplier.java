b88f8f9fbdd6499f482a4583af1da75a02488932
nahom <nbirhane@gmail.com>
2024-07-27 16:12:01 -0500

    supplier

--- src/main/java/miu/edu/ProductSystem/model/Supplier.java
+++ src/main/java/miu/edu/ProductSystem/model/Supplier.java
@@ -1,6 +1,24 @@
 package miu.edu.ProductSystem.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Supplier {
+    @Id
+    @GeneratedValue
+    private Integer supplierId;
+    @Column(name ="Supplier Name ")
+    private String name;
+    private String contactPhone;
 
-
+    @OneToMany(mappedBy = "product")
+    private List<Product> products;
 }
