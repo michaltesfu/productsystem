91c38763776b4fcb94ea3c1ee255321d94f4ddb0
michal tesfu <michaltesfu2@gmail.com>
2024-07-27 20:19:42 -0500

    javascript

--- src/main/resources/templates/js/scripts.js
+++ src/main/resources/templates/js/scripts.js
@@ -1,17 +1,16 @@
-// frontend/js/scripts.js
-
 document.addEventListener('DOMContentLoaded', function () {
     fetchProducts();
 });
 
 function fetchProducts() {
-    fetch('http://localhost:8080/srmweb/api/product/get/all')
+    fetch('http://localhost:8080/products/')
         .then(response => response.json())
         .then(data => {
             const productList = document.getElementById('productList');
             productList.innerHTML = '';
 
-            data.sort((a, b) => a.name.localeCompare(b.name)); // Sort by product name
+            // Sort by product name in ascending order
+            data.sort((a, b) => a.name.localeCompare(b.name));
 
             data.forEach((product, index) => {
                 const row = document.createElement('tr');
